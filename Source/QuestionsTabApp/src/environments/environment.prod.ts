ee4da821ab8ca1cd0ce6b0b4da27622f81e9dbd2
BGS-SysAdmin <54821993+BGS-SysAdmin@users.noreply.github.com>
2020-10-13 15:37:51 +1000

    Update environment.prod.ts

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://bgs-qbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://bgs-qbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "e0a762aa-f74f-473a-b086-4ceaefb71fbd",
+        clientId: "ba44a419-4680-44a5-af54-814f32272f85",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
